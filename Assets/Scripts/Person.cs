5b1f505a61260cbbf669e3579328b2ba4c889cfe
NelsonWang <wangnelson2@gmail.com>
2021-07-23 00:12:49 -0400

    Person | classtype

--- Assets/Scripts/Person.cs
+++ Assets/Scripts/Person.cs
@@ -3,19 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-enum EnumRaces {
-    Human, Elf, Orc, Drawf
+public enum EnumRaces
+{
+    Human, Elf, Orc, Dwarf
 }
+
+
+
+// TODO: Work on the class types later
+public enum classType
+{
+    Fighter, Swordman, Ranger, Archer, Assasin, Mechanic
+}
+
 
 public class Person
 {
-    string name, gender;
 
-    int age = 18;
+    string name, gender;
+    EnumRaces race = EnumRaces.Human;
+
+    int age = 18, level = 1;
     public Person(string nameIn)
     {
         name = nameIn;
 
+    }
+
+    public string getRace()
+    {
+
+        string[] races = { "Human", "Elf", "Orc", "Dwarf" };
+        return races[((int)race)];
+    }
+
+    public void levelUp()
+    {
+
+    }
+
+    public void oneYearOlder()
+    {
+        age++;
     }
 
 }
