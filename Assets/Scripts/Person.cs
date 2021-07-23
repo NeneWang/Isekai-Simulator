64dd474e375a6f04a4b5c7e9d1eba7d30b0e66fa
NelsonWang <wangnelson2@gmail.com>
2021-07-22 23:57:21 -0400

    Person | races

--- Assets/Scripts/Person.cs
+++ Assets/Scripts/Person.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Person : MonoBehaviour
+
+enum EnumRaces {
+    Human, Elf, Orc, Drawf
+}
+
+public class Person
 {
-    // Start is called before the first frame update
-    void Start()
+    string name, gender;
+
+    int age = 18;
+    public Person(string nameIn)
     {
-        
+        name = nameIn;
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
