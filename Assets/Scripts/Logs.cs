e9358fe14d0e878c897081d7ba126394a9354d01
NelsonWang <wangnelson2@gmail.com>
2021-07-25 22:19:14 -0400

    Logs | fame and logs

---
+++ Assets/Scripts/Logs.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogsList
+{
+
+
+
+}
+
+public enum EnumRarity
+{
+    Normal = 1, Rare = 2, Miracle = 3
+}
+
+public class Log
+{
+
+    public string title;
+    public EnumRarity enumRarity;
+    
+    public int goldOS, healthOS, happinessOS;
+
+
+
+
+}
