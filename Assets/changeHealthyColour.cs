ef1ad05aac18db4c5931d926a14c8ea9fcdacd1d
NelsonWang <wangnelson2@gmail.com>
2022-01-04 23:54:54 -0500

    m=main | colors change

---
+++ Assets/changeHealthyColour.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;using Naninovel;
+public class changeHealthyColour : MonoBehaviour
+{
+    // Target the heart colours
+    // Then change the colours depending on the score. Which since it has a trigger effect it calls here.
+
+    public Image rend;
+    public string variableName;
+
+    public void changeValue(int value){
+        changeColour(value);
+
+    }
+
+    public void changeValue(){
+        var variableManager = Engine.GetService<ICustomVariableManager>();
+var valueAsString = variableManager.GetVariableValue(variableName);
+        changeColour(int.Parse(valueAsString));
+
+    }
+
+    void changeColour(int value){
+        if(value>=75){
+            rend.color = Color.green;
+        }
+        else if(value>=40){
+            rend.color = Color.yellow;
+        }else{
+            // value less than 40
+            rend.color = Color.red;
+        }
+    }
+
+}
