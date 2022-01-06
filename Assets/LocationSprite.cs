aa02aa5705a52a9bc22871bc6d2393f34759f0a3
NelsonWang <wangnelson2@gmail.com>
2022-01-06 17:14:23 -0500

    sprites | housing

--- Assets/LocationSprite.cs
+++ Assets/LocationSprite.cs
@@ -1,13 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using Naninovel;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LocationSprite : MonoBehaviour
 {
-    public Sprite tentSprite, farmSprite, motelSprite, tavernSprite;
+    const int TENT = 0, FARM = 1, TAVERN = 2, CABIN = 3;
+
+    public Sprite
+
+            tentSprite,
+            farmSprite,
+            motelSprite,
+            tavernSprite;
+
     public string p_livingmethod;
 
-    public void updateBase(){
-        
+    public SpriteRenderer spriteRenderer;
+
+    public void updateBase()
+    {
+        var variableManager = Engine.GetService<ICustomVariableManager>();
+        var p_livingmethod = variableManager.GetVariableValue("p_livingmethod");
+
+        // then the
+        switch (int.Parse(p_livingmethod))
+        {
+            case TENT:
+                spriteRenderer.sprite = tentSprite;
+                break;
+            case FARM:
+                spriteRenderer.sprite = farmSprite;
+                break;
+            case TAVERN:
+                spriteRenderer.sprite = tavernSprite;
+                break;
+            case CABIN:
+                spriteRenderer.sprite = motelSprite;
+                break;
+        }
     }
 }
