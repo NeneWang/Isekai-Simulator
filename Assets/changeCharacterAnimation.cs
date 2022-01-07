d766b7a3082cabd99440a743011c029c3cdea07e
NelsonWang <wangnelson2@gmail.com>
2022-01-07 12:46:30 -0500

    sprites | change animation

---
+++ Assets/changeCharacterAnimation.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using Naninovel;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class changeCharacterAnimation : MonoBehaviour
+{
+    Animator animator;
+
+    void start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void updateAnimation()
+    {
+        var variableManager = Engine.GetService<ICustomVariableManager>();
+        var p_animation = variableManager.GetVariableValue("p_animation");
+        animator.SetInteger("p_animation", int.Parse(p_animation));
+    }
+}
