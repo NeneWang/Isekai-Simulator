631f26fb13a206c7f04a8eb113269c749fc1d031
NelsonWang <wangnelson2@gmail.com>
2022-01-12 19:34:07 -0500

    better choices | text change calendar better | fix

--- Assets/UpdateDate.cs
+++ Assets/UpdateDate.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Naninovel;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UpdateDate : MonoBehaviour
 {
@@ -9,8 +11,16 @@ public class UpdateDate : MonoBehaviour
 
     public void updateDate()
     {
-        NaniDataManager naniDataManager = new NaniDataManager();
+        var variableManager = Engine.GetService<ICustomVariableManager>();
+        var valueAsString = variableManager.GetVariableValue("p_turn");
 
+        if (valueAsString == null || valueAsString == "1")
+        {
+            return;
+        }
+        Debug.Log($"The Data var is {valueAsString}");
+
+        NaniDataManager naniDataManager = new NaniDataManager();
         string messageDate = naniDataManager.absoluteDateMessage;
         textMesh.text = messageDate;
     }
