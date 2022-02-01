1f0bfdd9b77a3c690bc5b49d8687679c9b336509
NelsonWang <wangnelson2@gmail.com>
2022-02-01 12:39:12 -0500

    =Constants

--- Assets/Scripts/CustomFunctions.cs
+++ Assets/Scripts/CustomFunctions.cs
@@ -356,7 +356,10 @@ public static class CustomFunctions
     {
         _ = printMenuChoices();
         _ = printMainStatus();
-        PlayScriptAsync("@set lastmenu=3\n@back bg-guild\n\"What will you be doing this week?\" [skipInput]\n@choice \"Work as Soldier\" if:last_career==1 goto:.workSoldier\n@choice \"Work as Aventurer\" if:last_career==2 goto:.workAventurer\n@choice \"Work as Merchant\" if:last_career==3 goto:workMerchant\n@choice \"Choose work\" gosub:.showJobMenu\n@choice \"Socialize\" gosub:.takeABreak");
+        PlayScriptAsync("@set lastmenu=3\n@back bg-guild\n\"What will you be doing this week?\" [skipInput]"+
+        "\n@choice \"Quest: Sabotage Demon Army\" if:quest==1 goto:.quest_1\n"+
+        "\n@choice \"Work as Soldier\" if:last_career==1 goto:.workSoldier\n@choice \"Work as Aventurer\" if:last_career==2 goto:.workAventurer\n@choice \"Work as Merchant\" if:last_career==3 goto:workMerchant\n"+
+        "@choice \"Choose work\" gosub:.showJobMenu\n@choice \"Socialize\" gosub:.takeABreak");
 
         return true;
     }
