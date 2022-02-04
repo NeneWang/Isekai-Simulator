5c4b48e749c75161d6d45c61107cf16f814f4d79
NelsonWang <wangnelson2@gmail.com>
2022-02-03 19:39:04 -0500

    =main storyline | 1

--- Assets/Scripts/CustomFunctions.cs
+++ Assets/Scripts/CustomFunctions.cs
@@ -358,6 +358,7 @@ public static class CustomFunctions
         _ = printMainStatus();
         PlayScriptAsync("@set lastmenu=3\n@back bg-guild\n\"What will you be doing this week?\" [skipInput]"+
         "\n@choice \"Quest: Sabotage Demon Army\" if:quest==1 goto:.quest_1\n"+
+        "\n@choice \"Quest: Go to the banquet\" if:quest==2 goto:.quest_2\n"+
         "\n@choice \"Work as Soldier\" if:last_career==1 goto:.workSoldier\n@choice \"Work as Aventurer\" if:last_career==2 goto:.workAventurer\n@choice \"Work as Merchant\" if:last_career==3 goto:workMerchant\n"+
         "@choice \"Choose work\" gosub:.showJobMenu\n@choice \"Socialize\" gosub:.takeABreak");
 
